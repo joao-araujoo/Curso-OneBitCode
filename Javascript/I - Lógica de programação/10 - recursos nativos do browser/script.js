d6b06a61121e5c81dbe23a145ccc7fc03d1443ea
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-05 18:19:29 -0300

    recursos nativos do browser

---
+++ Javascript/I - Lógica de programação/10 - recursos nativos do browser/script.js
@@ -0,0 +1,11 @@
+let nome = 'joao'
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert('olá, ' + nome)
+// nome = window.prompt('insira o seu nome:')
+let confirmação = window.confirm('Aceita esse termo?')
+
+console.log(confirmação)
