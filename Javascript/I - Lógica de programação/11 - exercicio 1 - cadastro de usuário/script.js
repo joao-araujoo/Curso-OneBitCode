2347f736b9f0634a89ea189c90a2798d42273a55
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-05 18:29:00 -0300

    exercicio 1 - cadastro de usuario

---
+++ Javascript/I - Lógica de programação/11 - exercicio 1 - cadastro de usuário/script.js
@@ -0,0 +1,7 @@
+let nome = prompt('Digite seu nome:')
+let sobrenome = prompt('Digite seu sobrenome:')
+let campoDeEstudo = prompt('Qual o seu campo de estudo?')
+let anoDeNascimento = prompt('Em que ano você nasceu?')
+let idade = 2022 - anoDeNascimento
+
+alert(`${nome} ${sobrenome}, ${campoDeEstudo}, ${idade} anos.`)
