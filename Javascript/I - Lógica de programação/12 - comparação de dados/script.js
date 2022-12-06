f324e4417e0db233d49343da30df314f71ea1bec
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-05 22:32:06 -0300

    comparação de dados

---
+++ Javascript/I - Lógica de programação/12 - comparação de dados/script.js
@@ -0,0 +1,14 @@
+// == e === igualdade
+// != e !== desigualdade
+// >, <, >=, <= comparação
+
+console.log(4 == 3) //false
+console.log('3' === 3) //false
+
+console.log(4 != 3) //true
+console.log('3' !== 3) //true
+
+console.log(3 > 4) //false
+console.log(3 < 4) //true
+console.log(4 >= 4) //true
+console.log(3 <= 4) //false
