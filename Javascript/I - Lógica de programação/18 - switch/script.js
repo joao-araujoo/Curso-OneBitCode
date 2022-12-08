a64fdf13d793c0799d07c3caa0cb2133d2cbe723
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-08 18:39:49 -0300

    switch

---
+++ Javascript/I - Lógica de programação/18 - switch/script.js
@@ -0,0 +1,15 @@
+const resposta = 'b'
+
+switch (resposta){
+    case 'a':
+        console.log('Resposta incorreta.')
+        break
+    case 'b':
+        console.log('Resposta correta!')
+        break
+    case 'c':
+        console.log('Resposta incorreta.')
+        break
+    default:
+        console.log('Alternativa inválida')
+}
