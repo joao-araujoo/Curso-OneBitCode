43075edc5329cb93ebca3ac79fcd7f33ccf8dfb7
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-08 10:00:05 -0300

    condições

---
+++ Javascript/I - Lógica de programação/16 - condições/script.js
@@ -0,0 +1,11 @@
+let idade = 5
+
+if (idade > 18){
+    console.log('Você é maior de idade')
+} else if (idade > 12){
+    console.log('Você é menor de idade')
+} else if (idade > 4){
+    console.log('Você é criança')
+} else {
+    console.log('...')
+}
