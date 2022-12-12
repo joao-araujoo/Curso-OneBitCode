5d4525c0cbdccb3c50200bae9a3de95dc05b2aa7
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-12 10:34:56 -0300

    do while

---
+++ Javascript/I - Lógica de programação/23 - do while/script.js
@@ -0,0 +1,8 @@
+let velocidade = 0
+
+do {
+    console.log('a velocidade do veículo é ' + velocidade + 'km/h')
+    velocidade -=20
+} while(velocidade > 0)
+
+console.log('velocidade final: ' + velocidade + 'km/h')
