4e45ea48bd39b8bde1b15af5fbe3e3380927accc
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-14 11:43:52 -0300

    trabalhando com arrays

--- Javascript/I - Lógica de programação/33 - trabalhando com arrays/script.js
+++ Javascript/I - Lógica de programação/33 - trabalhando com arrays/script.js
@@ -43,3 +43,25 @@ const anothers = arr.slice(-4)
 console.log(arr)
 console.log(hobbits)
 console.log(anothers)
+
+//CONCAT
+const society = hobbits.concat(anothers, 'Boromir')
+console.log(society)
+
+//Substituição dos elementos
+
+//SPLICE
+const removedElements = society.splice(index, 1, 'Gandalf, o Cinzento')
+console.log(society)
+console.log(removedElements)
+
+//Iterar sobre os Elementos
+for(let i = 0; i < society.length; i++){
+    const elemento = society[i]
+    console.log(elemento + ' se encontra na posição ' + i)
+}
+
+//Simplificado
+for(let pos in society){
+    console.log(society[pos])
+}
