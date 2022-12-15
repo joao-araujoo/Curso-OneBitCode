6b1350e4e9595eb38ce405ecb931dd35f480d945
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-15 10:43:10 -0300

    objeto

---
+++ Javascript/I - Lógica de programação/39 - objetos/script.js
@@ -0,0 +1,15 @@
+let pessoa = {
+    nome: 'João',
+    sobrenome: 'Araujo',
+    idade: 15,
+    endereço: {
+        rua: 'do limoeiro',
+        número: 21421521,
+        bairro: 'liminha'
+    }
+}
+
+console.log(pessoa)
+
+let localização = 'endereço'
+console.log(pessoa[localização])
