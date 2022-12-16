27d620f4b72bb7544aef9afb84c2b803635db9c8
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-16 17:22:41 -0300

    sexo e altura

--- Javascript/I - Lógica de programação/praticando/script.js
+++ Javascript/I - Lógica de programação/praticando/script.js
@@ -21,10 +21,32 @@ do {
             
             alert(`Maiores de idade: ${ofAge}\nMenores de idade: ${underAge}`)
             break
-        case '2':
-            break
-        case '3':
 
+        case '2':
+            let people = []
+            let masculine = 0
+            let female = 0
+            let biggerHeight = 0
+            let biggerHeightSex = ''
+
+            for(let x = 0; x <= 4; x++){
+                let sex = prompt(`Qual o sexo da ${x + 1}° pessoa? (M) - (F)`)
+                let height = Number(prompt(`Qual a altura da ${x + 1}° pessoa? (metros)`))
+                people.push({sex, height})
+            }
+
+            for(let pos in people){
+                people[pos].sex === 'M' ? masculine++ : female++
+                if(people[pos].height > biggerHeight){
+                    biggerHeight = people[pos].height
+                    biggerHeightSex = people[pos].sex
+                }
+            }
+
+            alert(`Homens: ${masculine}\nMulheres: ${female}\nMaior altura: ${biggerHeight} - ${biggerHeightSex}`)
+            break
+
+        case '3':
             let end3
             let names = [] 
             let namesText = ''
@@ -35,7 +57,7 @@ do {
                     inverseName += name[i]
                 }
                 names.push(inverseName)
-                end3 = confirm('Deseja continuar?')
+                end3 = confirm('Deseja adicionar outro nome?')
             }
             for(let pos in names){
                 namesText += `Nome ${Number(pos) + 1}: ${names[pos]}\n`
