e5db16f433baba3a2decc0b5842278daa31abdbc
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-20 10:27:33 -0300

    objetos globais

---
+++ Javascript/I - Lógica de programação/52 - objetos globais/script.js
@@ -0,0 +1,13 @@
+let pessoa = Object()
+let lista = Array('Cavalo', 'Cachorro', 'Gato')
+let string = String(console.log)
+let number = Number(undefined)
+
+console.log(Number.isInteger(4.5))
+
+let data = Date()
+console.log(data)
+
+console.log(Math.PI)
+console.log(Math.pow(2, 10))
+console.log(Math.sqrt(144))
