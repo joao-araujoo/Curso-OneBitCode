8e5b247b5846fde3840632662fcfcaecc885211a
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-26 13:41:03 -0300

    resolução - escalação de times

--- Javascript/II - DOM/07 - exercício 11 - escalação de time/script.js
+++ Javascript/II - DOM/07 - exercício 11 - escalação de time/script.js
@@ -4,23 +4,26 @@ function addPlayer(){
     const playersList = document.querySelector('.players-list')
 
     const ul = document.createElement('ul')
+
+    const numberLi = document.createElement('li')
+    const playerNumber = document.getElementById('player-number').value
+    numberLi.innerHTML = `<span class="number-${playerNumber}"><strong>Número:</strong> ${playerNumber}</span>`
+
     const h3 = document.createElement('h3')
-    h3.innerText = `Jogador ${players}`
+    h3.innerHTML = `<span class="number-${playerNumber}">Jogador ${players}</span>`
     ul.appendChild(h3)
     
     const positionLi = document.createElement('li')
     const playerPosition = document.getElementById('player-position').value
-    positionLi.innerHTML = `<strong>Posição:</strong> ${playerPosition}`
-    ul.appendChild(positionLi)
+    positionLi.innerHTML = `<span class="number-${playerNumber}"><strong>Posição:</strong> ${playerPosition}</span>`
 
     const nameLi = document.createElement('li')
     const playerName = document.getElementById('player-name').value
-    nameLi.innerHTML = `<strong>Nome:</strong> ${playerName}`
-    ul.appendChild(nameLi)
+    nameLi.innerHTML = `<span class="number-${playerNumber}"><strong>Nome:</strong> ${playerName}</span>`
 
-    const numberLi = document.createElement('li')
-    const playerNumber = document.getElementById('player-number').value
-    numberLi.innerHTML = `<strong>Número:</strong> ${playerNumber}`
+
+    ul.appendChild(positionLi)
+    ul.appendChild(nameLi)
     ul.appendChild(numberLi)
 
     playersList.appendChild(ul)
@@ -28,5 +31,10 @@ function addPlayer(){
 }
 
 function removePlayer(){
+    const playersList = document.querySelector('.players-list')
+    const numberSelected = document.querySelector('#r-player-number').value
+    const removingPlayer = document.getElementsByClassName(`.number-${numberSelected}`)
+    console.log(removingPlayer)
 
+    playersList.removeChild(removingPlayer)
 }
