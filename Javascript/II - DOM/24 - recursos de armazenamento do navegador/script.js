841c4c999456344ec113fc9825cd3652b95afb81
joao-araujoo <joao.araujoo2007@gmail.com>
2023-01-24 10:19:17 -0300

    recursos de armazenamento do navegador

---
+++ Javascript/II - DOM/24 - recursos de armazenamento do navegador/script.js
@@ -0,0 +1,40 @@
+document.getElementById('sessionBtn').addEventListener('click', function(){
+    const input = document.getElementById('session')
+    sessionStorage.setItem('info', input.value)
+    input.value = ''
+})
+
+document.getElementById('readSession').addEventListener('click', function(){
+    const info = sessionStorage.getItem('info')
+    alert('A informação salva é: ' + info)
+})
+
+document.getElementById('localBtn').addEventListener('click', function(){
+    const input = document.getElementById('local')
+    localStorage.setItem('text', input.value)
+    input.value = ''
+})
+
+document.getElementById('readLocal').addEventListener('click', function(){
+    const text = localStorage.getItem('text')
+    alert('O texto salvo é: ' + text)
+})
+
+document.getElementById('cookieBtn').addEventListener('click', function(){
+    const input = document.getElementById('cookie')
+    // cookieName=value; expires=UTCStringDate; path=/;
+    const cookie = 'info=' + input.value + ';'
+    const expiration = 'expires=' + new Date(2023, 1, 25) + ';'
+    const path = 'path=/;'
+    document.cookie = cookie + expiration + path
+    input.value = ''
+})
+
+document.getElementById('cookie2Btn').addEventListener('click', function(){
+    const input = document.getElementById('cookie2')
+    const cookie = 'text=' + input.value + ';'
+    const expiration = 'expires=' + new Date(2023, 2, 25) + ';'
+    const path = 'path=/;'
+    document.cookie = cookie + expiration + path
+    input.value = ''
+})
