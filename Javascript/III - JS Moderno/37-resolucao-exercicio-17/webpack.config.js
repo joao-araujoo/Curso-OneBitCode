c14d5144eb1d0841f64dae9f9a3c36f4c6ca2393
joao-araujoo <joao.araujoo2007@gmail.com>
2023-04-07 12:56:36 -0300

    webpack dev server

--- Javascript/III - JS Moderno/37-resolucao-exercicio-17/webpack.config.js
+++ Javascript/III - JS Moderno/37-resolucao-exercicio-17/webpack.config.js
@@ -1,6 +1,14 @@
 // webpack.config.js
+const path = require('path')
 
 module.exports = {
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist')
+      },
+      compress: true,
+      port: 8000
+    },
     entry: {
       index: './src/index.js'
     },
