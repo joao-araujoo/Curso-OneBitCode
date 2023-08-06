913b7a8b8cba7e8978213cce90270439b389c474
joao-araujoo <joao.araujoo2007@gmail.com>
2023-08-06 10:58:06 -0300

    bookstore - parte final

--- Javascript/IV - Programação Orientada a Objetos/22 - bookstore/App.js
+++ Javascript/IV - Programação Orientada a Objetos/22 - bookstore/App.js
@@ -1,6 +1,9 @@
 const Database = require("./Database")
 const Author = require("./entities/Author")
+const Order = require("./entities/Order")
+const Poster = require("./entities/Poster")
 const User = require("./entities/User")
+const Book = require("./entities/Book")
 
 module.exports = class App {
     static #database = new Database()
@@ -11,7 +14,7 @@ module.exports = class App {
     }
 
     getUsers(){
-        App.#database.find('users')
+        return App.#database.find('users')
     }
 
     createAuthor(name, nationality, bio){
@@ -19,7 +22,53 @@ module.exports = class App {
         App.#database.saveAuthor(author)
     }
 
-    getAuthor(){
-        App.#database.find('authors')
+    getAuthors(){
+        return App.#database.find('authors')
+    }
+
+    createBook(title, synopsis, genre, pages, author, description, price, inStock){
+        const book = new Book(title, synopsis, genre, pages, author, description, price, inStock)
+        App.#database.saveBook(book)
+    }
+
+    addBook(bookName, quantity){
+        App.#database.addBooksToStock(bookName, quantity)
+    }
+
+    getBooks(){
+        return App.#database.find('books')
+    }
+
+    createPoster(name, description, height, width, price, inStock){
+        const poster = new Poster(name, description, height, width, price, inStock)
+        App.#database.savePoster(poster)
+    }
+
+    addPoster(posterName, quantity){
+        App.#database.addPostersToStock(posterName, quantity)
+    }
+
+    getPosters(){
+        return App.#database.find('posters')
+    }
+
+    createOrder(items, user){
+        const order = new Order(items, user)
+        App.#database.saveOrder(order)
+        order.data.items.forEach(({product, quantity}) => {
+            if(product instanceof Book){
+                App.#database.removeBooksFromStock(product.name, quantity)
+            } else if(product instanceof Poster){
+                App.#database.removePostersFromStock(product.name, quantity)
+            }
+        })
+    }
+
+    getOrders(){
+        return App.#database.find('orders')
+    }
+
+    showDatabase(){
+        App.#database.showStorage()
     }
 }
