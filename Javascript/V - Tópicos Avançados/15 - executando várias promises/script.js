980c072474817afd490db57aa499ca0e0446cedd
joao-araujoo <joao.araujoo2007@gmail.com>
2023-08-14 17:22:07 -0300

    exemplo

--- Javascript/V - Tópicos Avançados/15 - executando várias promises/script.js
+++ Javascript/V - Tópicos Avançados/15 - executando várias promises/script.js
@@ -1,32 +1,32 @@
-// function asyncSum(a, b) {
-//     return new Promise((resolve, reject) => {
-//         if (typeof a !== 'number' || typeof b !== 'number') {
-//             reject('arguments must be of type number - função soma')
-//         } else {
-//             resolve(a + b)
-//         }
-//     })
-// }
+function asyncSum(a, b) {
+    return new Promise((resolve, reject) => {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            reject('arguments must be of type number - função soma')
+        } else {
+            resolve(a + b)
+        }
+    })
+}
 
-// function asyncSubtract(a, b) {
-//     return new Promise((resolve, reject) => {
-//         if (typeof a !== 'number' || typeof b !== 'number') {
-//             reject('arguments must be of type number - função subtração')
-//         } else {
-//             resolve(a - b)
-//         }
-//     })
-// }
+function asyncSubtract(a, b) {
+    return new Promise((resolve, reject) => {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            reject('arguments must be of type number - função subtração')
+        } else {
+            resolve(a - b)
+        }
+    })
+}
 
-// const sumResult = asyncSum(50, 33)
-// const subtractResult = asyncSubtract(50, 33)
+const sumResult = asyncSum(50, 33)
+const subtractResult = asyncSubtract(50, 33)
 
-// // entra com um array de promises e retorna um array de resultados
-// Promise.all([sumResult, subtractResult]).then(results => {
-//     results.forEach(result => console.log(result))
-// }).catch(err => {
-//     console.log(err)
-// })
+// entra com um array de promises e retorna um array de resultados
+Promise.all([sumResult, subtractResult]).then(results => {
+    results.forEach(result => console.log(result))
+}).catch(err => {
+    console.log(err)
+})
 
 
 const numbers = [4, 9, 5, null, 77]
