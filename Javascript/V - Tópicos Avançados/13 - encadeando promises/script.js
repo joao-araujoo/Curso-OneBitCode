27cfd78e93de119c6548c74eb63ed51c08a6958c
joao-araujoo <joao.araujoo2007@gmail.com>
2023-08-14 17:08:51 -0300

    usando retorno direto das arrow functions

--- Javascript/V - Tópicos Avançados/13 - encadeando promises/script.js
+++ Javascript/V - Tópicos Avançados/13 - encadeando promises/script.js
@@ -21,9 +21,7 @@ function getAge(birthday) {
 }
 
 getAge('2009-09-02')
-    .then(age => {
-        return checkAge(age)
-    })
+    .then(age => checkAge(age))
     .then(isOver18 => {
         if (isOver18) {
             console.log('Maior de idade')
