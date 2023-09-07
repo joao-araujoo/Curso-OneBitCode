3b01190f5ed691d228fec3999801a7d945a67572
joao-araujoo <joao.araujoo2007@gmail.com>
2023-09-07 16:48:19 -0300

    tuplas

---
+++ Typescript/10 - tuplas/index.ts
@@ -0,0 +1,5 @@
+// tuplas são vetores imutáveis que possuem seus tipos predefinidos em sua criação.
+let point: [number, number, string, boolean]
+point = [2, 5, 'Test', true]
+
+let [x, y, msg, condition] = point
