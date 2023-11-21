8a2f7441d3b2154af1b1a9753f7a45e04da034d9
joao araujo <joao.araujoo2007@gmail.com>
2023-11-21 18:14:08 -0300

    8 - criando um projeto React com Vite

---
+++ React/8 - criando um projeto react com vite/first-react-project/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Olá, Mundo!</h1>
+      <p>Este é meu primeiro projeto usando React e Vite</p>
+      <p>
+        <a href="https://vitejs.dev/guide/features.html">Vite Features</a>
+      </p>
+    </div>
+  )
+}
