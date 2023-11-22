be14c68142c91946942572d85af320699da309f5
joao araujo <joao.araujoo2007@gmail.com>
2023-11-22 14:19:45 -0300

    12 - criando novos componentes

---
+++ React/12 - criando novos componentes/react-fundamentals/src/components/StatusText.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line react-refresh/only-export-components, react/display-name
+export default () => {
+    const status = true
+
+    return <h2>Current status: {status ? 'on' : 'off'}</h2>
+}
