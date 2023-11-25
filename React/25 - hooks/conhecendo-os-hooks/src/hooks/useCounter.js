daa23889fe2e0ead7da41c865a6d8a23efcde4b0
joao araujo <joao.araujoo2007@gmail.com>
2023-11-25 14:07:17 -0300

    26 - conhecendo melhor o useState

--- React/25 - hooks/conhecendo-os-hooks/src/hooks/useCounter.js
+++ React/25 - hooks/conhecendo-os-hooks/src/hooks/useCounter.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
 
+function getInitialValue() {
+  console.log("Obtendo o valor inicial...")
+  return 1 + 1
+}
+
 export default function useCounter() {
-  const [count, setCount] = useState(0);
+  // se passarmos uma função como parâmetro para o useState, ele apenas a utilizará uma única vez, porque ele entende que só deve ser executada para obter o estado inicial
+  const [count, setCount] = useState(() => getInitialValue());
 
   const increment = () => {
-    setCount(count + 1);
+    // o código abaixo não funciona como o esperado, uma vez que o React executa de forma assíncrona, então, é como se o valor "count" fosse igual na hora da execução das mesmas
+    // setCount(count + 1); // 2 + 1
+    // setCount(count + 1); // 2 + 1
+
+    // quando for necessário utilizar o valor para atualizar o state, é correto fazer da maneira abaixo
+    setCount((currentState) => currentState + 2);
+    // o método acima força o React a utilizar o valor atual, diferente da abordagem anterior
   };
 
   return { count, increment };
