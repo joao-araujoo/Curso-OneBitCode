782964ddff7513e5b70543b86c676936ee1e5437
joao araujo <joao.araujoo2007@gmail.com>
2023-12-30 11:12:35 -0300

    11 - primeira rota com o Express

--- Node.js/to-do-list/app.js
+++ Node.js/to-do-list/app.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const PORT = 3000
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Minha lista de Tarefas =)</h1>');
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
