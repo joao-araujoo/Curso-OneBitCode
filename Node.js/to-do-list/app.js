bddb488d0361a4c7eccb1542522038f5ca3c5020
joao araujo <joao.araujoo2007@gmail.com>
2023-12-31 11:35:32 -0300

    12 - respostas JSON

--- Node.js/to-do-list/app.js
+++ Node.js/to-do-list/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const PORT = 3000
+const PORT = 3000;
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('<h1>Minha lista de Tarefas =)</h1>');
+})
+
+app.get('/json', (req, res) => {
+  res.json({ title: "Tarefa X", done: true });
 })
 
 app.listen(PORT, () => {
