96ab1e0442c4e21a2138f29413294f23107fa1d2
joao araujo <joao.araujoo2007@gmail.com>
2023-12-31 11:50:58 -0300

    15 - middlewares

--- Node.js/to-do-list/app.js
+++ Node.js/to-do-list/app.js
@@ -3,6 +3,18 @@ const PORT = 3000;
 
 const app = express();
 
+// middleware utilizado para proporcionar corretamente o json no corpo da requisição e tornar o "req.body" utilizável
+app.use(express.json());
+
+// o next é o parâmetro que permite que o middleware passe para o próximo, no método chamado "chain of responsability" 
+const log = (req, res, next) => {
+  console.log(req.body);
+  console.log(`Data: ${Date.now()}`);
+  next();
+}
+
+app.use(log)
+
 app.get('/', (req, res) => {
   res.send('<h1>Minha lista de Tarefas =)</h1>');
 })
