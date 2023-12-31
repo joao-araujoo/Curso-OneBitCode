ae97cca9dd567d669ff0a4d3c416f8843c7fd818
joao araujo <joao.araujoo2007@gmail.com>
2023-12-31 12:00:45 -0300

    16 - concentrando rotas

--- Node.js/to-do-list/app.js
+++ Node.js/to-do-list/app.js
@@ -1,27 +1,13 @@
 const express = require('express');
+const checkListRouter = require('./src/routes/checklist');
 const PORT = 3000;
 
 const app = express();
 
-// middleware utilizado para proporcionar corretamente o json no corpo da requisição e tornar o "req.body" utilizável
 app.use(express.json());
 
-// o next é o parâmetro que permite que o middleware passe para o próximo, no método chamado "chain of responsability" 
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log(`Data: ${Date.now()}`);
-  next();
-}
-
-app.use(log)
-
-app.get('/', (req, res) => {
-  res.send('<h1>Minha lista de Tarefas =)</h1>');
-})
-
-app.get('/json', (req, res) => {
-  res.json({ title: "Tarefa X", done: true });
-})
+// podemos utilizar o parâmetro para o caminho, sendo assim, todas as rotas do arquivo importado iniciarão com tal caminho especificado
+app.use('checklists', checkListRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
