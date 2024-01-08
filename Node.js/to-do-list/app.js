66e53e1d0837cc25dd6ea3f5cbcec82f4abf0b1b
joao araujo <joao.araujoo2007@gmail.com>
2024-01-08 10:32:57 -0300

    14 - EJS e configuração com express

--- Node.js/to-do-list/app.js
+++ Node.js/to-do-list/app.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const path = require("path");
 const checkListRouter = require("./src/routes/checklist");
+const rootRouter = require("./src/routes/index");
 const connectToDatabase = require("./config/database");
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
-
 // podemos utilizar o parâmetro para o caminho, sendo assim, todas as rotas do arquivo importado iniciarão com tal caminho especificado
 app.use("/checklists", checkListRouter);
+app.use("/", rootRouter);
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.set("view engine", "ejs");
 
 try {
   connectToDatabase();
